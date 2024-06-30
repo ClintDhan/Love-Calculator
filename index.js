a3c4a004a04606237bacc3248740dadc1e761751
ClintDhan <dhaniellerono@gmail.com>
2024-06-30 21:02:44 +0800

    tempo

---
+++ index.js
@@ -0,0 +1,5 @@
+let firstName = prompt("Enter first name:");
+let secondName = prompt("Enter second name:");
+let percentage = Math.random();
+percentage = Math.floor((percentage * 100)) + 1;
+alert(firstName + " and " + secondName + " have " + percentage + "% chance to be together");
